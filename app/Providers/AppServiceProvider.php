f06c61c14ccc770470f78bf7c5aaffad3e0e0cf7
Ekramul Islam Sumon <mmsumon799@gmail.com>
2022-09-15 16:35:28 +0600

    Test query execution time

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /*DB::listen(function ($query) {
+            Log::info($query->sql);     // the query being executed
+            Log::info($query->time);    // query time in milliseconds
+        });*/
     }
 }
