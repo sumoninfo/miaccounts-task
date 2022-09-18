2433c7e58a7c93431b23f38d338da8a15181a39c
Ekramul Sumon <38817030+sumoninfo@users.noreply.github.com>
2022-09-19 01:12:55 +0600

    Report 2 working on

--- frontend/src/views/report/Report2.vue
+++ frontend/src/views/report/Report2.vue
@@ -4,6 +4,9 @@
         <thead>
         <tr>
             <th scope="col">Acc Head id</th>
+            <th scope="col">G. Lvl 1</th>
+            <th scope="col">G. Lvl 2</th>
+            <th scope="col">G. Lvl 3</th>
             <th scope="col">Acc Head</th>
             <th scope="col">Total</th>
         </tr>
@@ -12,6 +15,16 @@
         <tr v-for="(list, index) in lists">
             <th scope="row">{{ list.id }}</th>
             <td>{{ list.name }}</td>
+                <ChildGroup class_name="table table-bordered" v-if="list.childrens.length>0" :childrens="list.childrens" />
+<!--                <span v-if="list.childrens.length>0">{{ list.childrens[0].name}}</span>-->
+
+            <td>
+
+                <span v-if="list.childrens.length>0 && list.childrens[0].childrens.length>0">{{ list.childrens[0].childrens[0].name}}</span>
+            </td>
+            <td>
+                <AccountHead class_name="table table-bordered" v-if="list.childrens.length>0 && list.childrens[0].childrens.length>0" :account_heads="list.childrens[0].childrens[0].account_heads"/>
+             </td>
             <td>{{ list.total_amounts }}</td>
         </tr>
         </tbody>
@@ -21,9 +34,12 @@
 <script>
 import {onMounted, ref} from "vue";
 import ApiService       from "../../services/api.service.js";
+import AccountHead      from "../../components/AccountHead.vue";
+import ChildGroup       from "../../components/ChildGroup.vue";
 
 export default {
     name: "Report1",
+    components: {ChildGroup, AccountHead},
     setup() {
         let lists           = ref([])
         let errors          = ref('')
