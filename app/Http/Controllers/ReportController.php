957867a138137be4a83627c02caed9f13d311fd8
Ekramul Islam Sumon <mmsumon799@gmail.com>
2022-09-19 11:51:11 +0600

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -32,30 +32,31 @@ class ReportController extends Controller
      */
     public function report2()
     {
-        $query = AccountHead::with('group.parent.parent')->addSelect(['total_amounts' => Transaction::query()
-            ->whereColumn('account_head_id', 'account_heads.id')
-            ->selectRaw('sum(debit-credit) as total_amounts')
-        ])->get()->map(function (AccountHead $accountHead) {
-            $group_1 = $group_2 = $group_3 = '';
-            if (isset($accountHead->group->parent->parent)) {
-                $group_1 = $accountHead->group->parent->parent->name;
-                $group_2 = $accountHead->group->parent->name;
-                $group_3 = $accountHead->group->name;
-            } elseif (isset($accountHead->group->parent)) {
-                $group_1 = $accountHead->group->parent->name;
-                $group_2 = $accountHead->group->name;
-            } elseif (isset($accountHead->group)) {
-                $group_1 = $accountHead->group->name;
-            }
-            return [
-                'id'            => $accountHead->id,
-                'name'          => $accountHead->name,
-                'total_amounts' => $accountHead->total_amounts,
-                'group_1'       => $group_1,
-                'group_2'       => $group_2,
-                'group_3'       => $group_3,
-            ];
-        });
+        $query = AccountHead::with('group.parent.parent')
+            ->addSelect(['total_amounts' => Transaction::query()
+                ->whereColumn('account_head_id', 'account_heads.id')
+                ->selectRaw('sum(debit-credit) as total_amounts')
+            ])->get()->map(function (AccountHead $accountHead) {
+                $group_1 = $group_2 = $group_3 = '';
+                if (isset($accountHead->group->parent->parent)) {
+                    $group_1 = $accountHead->group->parent->parent->name;
+                    $group_2 = $accountHead->group->parent->name;
+                    $group_3 = $accountHead->group->name;
+                } elseif (isset($accountHead->group->parent)) {
+                    $group_1 = $accountHead->group->parent->name;
+                    $group_2 = $accountHead->group->name;
+                } elseif (isset($accountHead->group)) {
+                    $group_1 = $accountHead->group->name;
+                }
+                return [
+                    'id'            => $accountHead->id,
+                    'name'          => $accountHead->name,
+                    'total_amounts' => $accountHead->total_amounts,
+                    'group_1'       => $group_1,
+                    'group_2'       => $group_2,
+                    'group_3'       => $group_3,
+                ];
+            });
         return response()->json($query);
     }
 }
